6c322fb29093f62bb4755ac541141fbc50f3d519
Aman Jaiswal <aman.jaiswal@code.berlin>
2025-10-13 20:18:58 +0200

    minor design

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -33,9 +33,9 @@ export default function NavBar({ onNavigateToLumio, onNavigateToRizing, onNaviga
                     className="bg-[#e9e9e9] box-border content-stretch flex flex-row gap-px h-11 items-center justify-center p-0 relative rounded-[13px] shrink-0 w-[160px] sm:w-[181px] hover:bg-[#ddd] transition-colors"
                 >
                     <div className="font-['Helvetica_Neue:Medium',_sans-serif] h-8 leading-[0] not-italic relative shrink-0 text-[#8e8e8e] text-[16px] sm:text-[18px] text-left flex-1 text-center">
-                        <p className="block leading-[32px]">Case Studies</p>
+                        <p className="block leading-[28px] sm:leading-[32px]">Case Studies</p>
                     </div>
-                    <div className={`relative shrink-0 mr-2 mt-1 size-6 sm:mt-0 sm:size-8 transition-transform ${isDropdownOpen ? 'rotate-180' : ''}`}>
+                    <div className={`relative shrink-0 mr-2 size-6 sm:size-8 transition-transform ${isDropdownOpen ? 'rotate-180' : ''}`}>
                         <svg className="block max-w-none size-full" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M6 9L12 15L18 9" stroke="#8e8e8e" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                         </svg>
